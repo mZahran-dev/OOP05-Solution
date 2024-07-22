b0e5477761457fe499a6ccd06c41e8dbdfefa211
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-22 17:59:53 +0300

    Operators Overloading

---
+++ OOP05Demo/Operator Overloading/Complex.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP05Demo.Operator_Overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+        //Function must be non-private and static [Class member method]
+
+        //binary operator
+        public static Complex operator +(Complex left,  Complex right)
+        {
+            return new Complex 
+            {
+                Real = (left?.Real ?? 0 + right?.Real?? 0),
+                Imag = (left?.Imag ?? 0 + right?.Imag ?? 0)
+            };
+
+        }
+       
+        public static Complex operator -(Complex left, Complex right)
+        {
+            return new Complex
+            {
+                Real = (left?.Real ?? 0 - right?.Real ?? 0),
+                Imag = (left?.Imag ?? 0 - right?.Imag ?? 0)
+            };
+
+        }
+
+        //unary operator
+        public static Complex operator ++(Complex complex)
+        {
+            return new Complex
+            {
+                Real = (complex?.Real ?? 0 ) + 1,
+                Imag = (complex?.Imag ?? 0)  
+            };
+
+        }
+        
+        //Comparison operators
+        public static bool operator > (Complex left, Complex right)
+        {
+            if(left?.Real == right?.Real) 
+                return left?.Imag > right?.Imag;
+            else
+                return left?.Real > right?.Real;
+        }
+        public static bool operator < (Complex left, Complex right)
+        {
+            if (left?.Real == right?.Real)
+                return left?.Imag < right?.Imag;
+            else
+                return left?.Real < right?.Real;
+        }
+
+        //casting operators
+        public static explicit operator int(Complex complex)
+        {
+            return complex?.Real??0;
+        }
+        public static implicit operator string(Complex complex)
+        {
+            return complex?.ToString() ?? string.Empty;
+        }
+
+    }
+}
--- OOP05Demo/Program.cs
+++ OOP05Demo/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace OOP05Demo
+﻿using OOP05Demo.Operator_Overloading;
+using System.Net.Http.Headers;
+
+namespace OOP05Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Complex c1 = new Complex() { Real = 3 , Imag = 4 };
+            Complex c2 = new Complex() { Real = 2, Imag = 7 };
+
+            #region Binary operators
+            //Complex c3 =  c1 + c2;
+            //Complex c4 = c1 - c3;
+            //Console.WriteLine(c3); 
+            #endregion
+
+            #region Unary operators
+            //Complex c3 = c2;
+            //Console.WriteLine("Before : ");
+            //Console.WriteLine(c1.GetHashCode());
+            //Console.WriteLine(c3.GetHashCode());
+
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c3);
+
+            //c3 = c1++; // will create a new object
+            //Console.WriteLine("After post fix");
+            //Console.WriteLine(c1.GetHashCode());
+            //Console.WriteLine(c3.GetHashCode());
+
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c3);
+
+            //c3 = ++c1; 
+
+            //Console.WriteLine("After prefix: ");
+            //Console.WriteLine(c1.GetHashCode());
+            //Console.WriteLine(c3.GetHashCode());
+
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c3); 
+            #endregion
+
+            #region comparison operator
+            //if (c1 > c2)
+            //    Console.WriteLine("c1 is greater");
+            //else if (c2 > c1)
+            //    Console.WriteLine("c2 is greater");
+            //else
+            //    Console.WriteLine("Both are equal");
+            #endregion
+                    
+            #region casting operator overloading
+            //long x = 50;
+            //int y = (int)x; // casting operator
+            //int z = (int)c1;
+            //Console.WriteLine(z);
+
+            //string str = c2; // implicit
+            //Console.WriteLine(str); 
+            #endregion
+
+
+
+
         }
     }
 }
