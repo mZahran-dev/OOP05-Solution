e85d348fd8602ecd201e74e167d421b957b4eded
DELL <DELL@DESKTOP-M4KTLBQ>
2024-07-23 01:05:14 +0300

    Delivering Assignment

---
+++ OOP05Assignment/First Project/3DPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP05Assignment.First_Project
+{
+    internal class _3DPoint: ICloneable
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; } 
+        #endregion
+
+        #region Constructors
+        public _3DPoint() : this(0, 0, 0) { }
+        public _3DPoint(double _x) : this(_x, 0, 0) { }
+
+        public _3DPoint(double _x, double _y) : this(_x, _y, 0) { }
+        public _3DPoint(double _x, double _y, double _z)
+        {
+            X = _x;
+            Y = _y;
+            Z = _z;
+        } 
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Point3D Coordinates (X: {X}, Y: {Y}, Z: {Z})";
+        }
+
+        public object Clone()
+        {
+            return new _3DPoint(X, Y, Z);
+        }
+
+        #region Comparison Operator overloading
+        public static bool operator ==(_3DPoint a, _3DPoint b)
+        {
+            return (a.X == b.X && a.Y == b.Y && a.Z == b.Z);
+        }
+        public static bool operator !=(_3DPoint a, _3DPoint b)
+        {
+            return (a.X != b.X || a.Y != b.Y || a.Z != b.Z);
+        } 
+        #endregion
+
+
+    }
+}
---
+++ OOP05Assignment/Second Project/Maths.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP05Assignment.Second_Project
+{
+    internal static class Maths
+    { 
+        public static double Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+            
+        }
+
+        public static double Multiply(int num1, int num2)
+        { 
+            return num1 * num2; 
+        }
+
+        public static double Divide(int num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
+    }
+}
